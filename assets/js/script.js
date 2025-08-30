a15c321b808ab1a9e273b865400cd25f18868c73
Fanuel Hindeya <154921331+fanuelH@users.noreply.github.com>
2025-08-30 10:41:44 +0300

    chore(script): add possible winner combinations

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,14 @@ let gameBoard = {
 };
 
 const marker = ["X", "O"];
+
+const winningCombos = [
+  [gameBoard.row1.col1, gameBoard.row1.col2, gameBoard.row1.col3],
+  [gameBoard.row2.col1, gameBoard.row2.col2, gameBoard.row2.col3],
+  [gameBoard.row3.col1, gameBoard.row3.col2, gameBoard.row3.col3],
+  [gameBoard.row1.col1, gameBoard.row2.col1, gameBoard.row3.col1],
+  [gameBoard.row1.col2, gameBoard.row2.col2, gameBoard.row3.col2],
+  [gameBoard.row1.col3, gameBoard.row2.col3, gameBoard.row3.col3],
+  [gameBoard.row1.col1, gameBoard.row2.col2, gameBoard.row3.col3],
+  [gameBoard.row1.col3, gameBoard.row2.col2, gameBoard.row3.col1],
+];
