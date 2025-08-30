1abd80a72b1408b8f8e4d0224250754d48a7eb0d
Fanuel Hindeya <154921331+fanuelH@users.noreply.github.com>
2025-08-30 10:40:27 +0300

    chore(script): initialize game board and markers

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const playerOneMark = [];
+const PlayerTwoMark = [];
+
+let playerTurn = 0;
+
+let gameBoard = {
+  row1: { col1: "", col2: "", col3: "" },
+  row2: { col1: "", col2: "", col3: "" },
+  row3: { col1: "", col2: "", col3: "" },
+};
+
+const marker = ["X", "O"];
